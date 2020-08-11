e7703d6a62e49eecf48d41a0369924b8195b3cbe
Christine <huangc909@gmail.com>
2020-08-10 22:58:52 -0400

    Stylized Book Page and buttons

--- src/components/routes/Book.js
+++ src/components/routes/Book.js
@@ -69,31 +69,65 @@ const Book = (props) => {
   console.log(setBook)
 
   return (
-    <div>
-      <h2>{book.title}</h2>
-      <h4>by {book.author}</h4>
-      <p>{book.rating}</p>
-      <p>{book.note}</p>
-      <div>
-        <p>Added to Wishlist</p>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 book-image-div">
+          <div className="col-md-6 align-right">
+            <img src="https://user-images.githubusercontent.com/53062479/89836158-2a23e800-db34-11ea-8fd4-983f002de819.jpg" className="book-image"/>
+
+            <div className="checkbox-info">
+              <div className="container">
+                <div className="row wishlist-row">
+                  <div className="wishlist">
+                    <p>Added to Wishlist</p>
+                  </div>
+                  <div className="wishlist-box">
+                    <div className="wishlist-marker">
+                      {book.onWishlist ? 'X' : ''}
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="container">
+                <div className="row readlist-row">
+                  <div className="readlist">
+                    <p>Marked as Read</p>
+                  </div>
+                  <div className="readlist-box">
+                    <div className="readlist-marker">
+                      {book.onRead ? 'X' : ''}
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+        <div className="col-md-6 book-detail">
+          <h2>{book.title}</h2>
+          <h4>by {book.author}</h4>
+          <p>Rating: {book.rating}/5.0</p>
+          <br />
+
+          <p>Notes:</p>
+          <p>{book.note}</p>
+
+        </div>
       </div>
-      <div>
-        {book.onWishlist ? 'X' : ''}
-      </div>
-      <p>Marked as Read</p>
-      <div>
-        {book.onRead ? 'X' : ''}
-      </div>
-      <div>
-        <button className="button btn btn-danger" onClick={destroy}>Delete Book</button>
-        <Link to={`/books/${props.match.params.bookId}/edit`}
-          book={book}
-        >
-          <button className="button btn btn-success">Edit Book</button>
-        </Link>
-      </div>
-      <div>
-        <Link to='/books/'>Back to all books</Link>
+      <div className="buttons">
+        <div>
+          <Link to={`/books/${props.match.params.bookId}/edit`}
+            book={book}
+          >
+            <button className="button btn btn-success">Edit Book</button>
+          </Link>
+          <button className="button btn btn-danger" onClick={destroy}>Delete Book</button>
+        </div>
+        <div>
+          <Link to='/books/'>Back to all books</Link>
+        </div>
       </div>
     </div>
   )
