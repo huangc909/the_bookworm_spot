b52ee109aceb04b54ad0401b861bcf3ed2193f47
Christine <huangc909@gmail.com>
2020-08-11 21:43:24 -0400

    Created AllBooks and AllBooksDetail Components

---
+++ src/components/routes/AllBooks.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+
+import apiUrl from './../../apiConfig'
+
+import messages from './../AutoDismissAlert/messages'
+
+const AllBooks = (props) => {
+  const [books, setBooks] = useState([])
+
+  const { msgAlert } = props
+  console.log(props)
+  useEffect(() => {
+    console.log(props)
+    axios({
+      method: 'GET',
+      url: `${apiUrl}/all-books/`,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => {
+        console.log(res)
+        return res
+      })
+      .then(res => {
+        console.log(res)
+        return res
+      })
+      .then(res => setBooks(res.data))
+      .catch(error => {
+        setBooks({ title: '', author: '' })
+        msgAlert({
+          head: 'Failed to show all books ' + error.message,
+          message: messages.showBooksFailure,
+          variant: 'danger'
+        })
+      })
+  }, [])
+  console.log(books)
+
+  // const showBook = (props) => {
+  //   books.map(book => {
+  //     if (props.user.id === book.owner) {
+  //       return '/books/'
+  //     } else if (props.user.id !== book.owner || props.user === null) {
+  //       return '/all-books-detail/'
+  //     }
+  //   })
+  // }
+
+  const booksJsx = books.reverse().map(book => (
+    <li key={book.id}>
+      <img src="https://user-images.githubusercontent.com/53062479/89836158-2a23e800-db34-11ea-8fd4-983f002de819.jpg" className="book-image"/>
+      <Link to={`/all-books-detail/${book.id}/`}>{book.title}</Link>
+    </li>
+  ))
+
+  // const booksJsx = books.reverse().map(book => (
+  //   <li key={book.id}>
+  //     <img src="https://user-images.githubusercontent.com/53062479/89836158-2a23e800-db34-11ea-8fd4-983f002de819.jpg" className="book-image"/>
+  //     <Link to={/books/`${book.id}/`}>{book.title}</Link>
+  //   </li>
+  // ))
+
+  return (
+    <div>
+      <h4>Book Recently Added</h4>
+      <div>
+        <ol>
+          {booksJsx}
+        </ol>
+      </div>
+    </div>
+  )
+}
+
+export default AllBooks
---
+++ src/components/routes/AllBooksDetail.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+import apiUrl from '../../apiConfig'
+
+import messages from './../AutoDismissAlert/messages'
+const AllBooksDetail = (props, cancelPath) => {
+  const [book, setBook] = useState(null)
+  const { msgAlert } = props
+  console.log(props)
+  useEffect(() => {
+    axios({
+      url: `${apiUrl}/all-books-detail/${props.match.params.bookId}/`,
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => setBook(res.data))
+      .then(() => msgAlert({
+        heading: 'Showing selected book',
+        message: messages.showAllBooksDetailSuccess,
+        variant: 'primary'
+      }))
+      .catch(error => {
+        setBook({ title: '', author: '' })
+        msgAlert({
+          heading: 'Failed to show book ' + error.message,
+          message: messages.showAllBooksDetailFailure,
+          variant: 'danger'
+        })
+      })
+  }, [])
+
+  if (!book) {
+    return <p>Loading...</p>
+  }
+  console.log(book)
+  console.log(setBook)
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 book-image-div">
+          <div className="col-md-6 align-right">
+            <img src="https://user-images.githubusercontent.com/53062479/89836158-2a23e800-db34-11ea-8fd4-983f002de819.jpg" className="book-image"/>
+          </div>
+        </div>
+
+        <div className="col-md-6 book-detail">
+          <h2>{book.title}</h2>
+          <h4>by {book.author}</h4>
+          <p>Rating: {book.rating}/5.0</p>
+          <br />
+        </div>
+        <div className="col-md-12 center">
+          <Link to='/all-books/'>
+            <p>Go Back</p>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AllBooksDetail
