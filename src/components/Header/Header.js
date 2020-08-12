4adb3cbbf50c4843ab03839ba693d0b316187b0e
Christine <huangc909@gmail.com>
2020-08-11 21:45:37 -0400

    Added drop-down menu, added logo, & updated .brand href to all-books

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,35 +1,47 @@
 import React, { Fragment } from 'react'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
+import Dropdown from 'react-bootstrap/Dropdown'
+import DropdownButton from 'react-bootstrap/DropdownButton'
 
 const authenticatedOptions = (
-  <Fragment>
-    <Nav.Link href="#books/">See All Books</Nav.Link>
-    <Nav.Link href="#wishlist/">See Wishlist</Nav.Link>
-    <Nav.Link href="#readbooks/">See Read Books</Nav.Link>
-    <Nav.Link href="#change-password/">Change Password</Nav.Link>
-    <Nav.Link href="#sign-out/">Sign Out</Nav.Link>
-  </Fragment>
+  // <Fragment>
+  //   <Nav.Link href="#books/">See My Books</Nav.Link>
+  //   <Nav.Link href="#wishlist/">See Wishlist</Nav.Link>
+  //   <Nav.Link href="#readbooks/">See Read Books</Nav.Link>
+  //   <Nav.Link href="#change-password/">Change Password</Nav.Link>
+  //   <Nav.Link href="#sign-out/">Sign Out</Nav.Link>
+  // </Fragment>
+  <DropdownButton
+    alignRight
+    title="My Account"
+    id="dropdown-menu-align-right"
+  >
+    <Dropdown.Item href="#books/">My Books</Dropdown.Item>
+    <Dropdown.Item href="#wishlist/">Wishlist</Dropdown.Item>
+    <Dropdown.Item href="#readbooks/">Read Books</Dropdown.Item>
+    <Dropdown.Item href="#change-password/">Change Password</Dropdown.Item>
+    <Dropdown.Item href="#sign-out/">Sign Out</Dropdown.Item>
+  </DropdownButton>
 )
 
 const unauthenticatedOptions = (
   <Fragment>
-    <Nav.Link href="#sign-up/">Sign Up</Nav.Link>
+    {/* <Nav.Link href="#sign-up/">Sign Up</Nav.Link> */}
     <Nav.Link href="#sign-in/">Sign In</Nav.Link>
   </Fragment>
 )
 
 const Header = ({ user }) => (
   <Navbar bg="primary" variant="dark" expand="md">
-    <Navbar.Brand href="#books/">
-      <img src="https://user-images.githubusercontent.com/53062479/89750722-2c863380-da9b-11ea-9da6-89b6cd205fa3.gif" alt="Logo" className='logo'/>
+    <Navbar.Brand href="#all-books/">
+      <img src="https://user-images.githubusercontent.com/53062479/89816393-824af200-db14-11ea-83cc-42714d271694.gif" alt="Logo" className='logo'/>
       The BookWorm Spot
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="ml-auto">
         { user && <span className="navbar-text mr-2">Welcome, {user.email}</span>}
-
         { user ? authenticatedOptions : unauthenticatedOptions }
       </Nav>
     </Navbar.Collapse>
