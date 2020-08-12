04dedadc3a5640926fdd80c02b87a01cb3205f53
Christine <huangc909@gmail.com>
2020-08-12 08:38:29 -0400

    Updated sign-up title to h1

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -53,7 +53,7 @@ class SignUp extends Component {
       <div className="list-style">
         <div className="row">
           <div className="col-sm-10 col-md-8 mx-auto mt-5">
-            <h3>Sign Up</h3>
+            <h1>Sign Up</h1>
             <Form onSubmit={this.onSignUp}>
               <Form.Group controlId="email">
                 <Form.Control
